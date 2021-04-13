f93a0f7233ca6ec2f7c7a1a80c5f344e6302e18e
Diana <dianaliviz2001@gmail.com>
2021-04-13 12:05:35 +0300

    explosions with sound and particles

--- objects/Asteroid.gd
+++ objects/Asteroid.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 signal explode
 
 var asteroid_small_scene := load("res://objects/AsteroidSmall.tscn")
+var explosion_particles_scene := load("res://objects/ParticlesAsteroidExplosion.tscn")
 var rng = RandomNumberGenerator.new()
 var is_exploded = false
 
@@ -16,6 +17,9 @@ func explode():
 		return
 		
 	is_exploded = true
+	
+	_explosion_particles()
+	_play_explosion_sound()
 	
 	emit_signal("explode")
 	
@@ -50,3 +54,17 @@ func _randomize_trajectory(asteroid):
 	# random direction
 	asteroid.linear_velocity = Vector2(lv_x * 400, lv_y * 400)
 	asteroid.linear_damp = 0
+
+func _explosion_particles():
+	var explosion_particles = explosion_particles_scene.instance()
+	explosion_particles.position = self.position
+	get_parent().add_child(explosion_particles)
+	explosion_particles.emitting = true
+
+func _play_explosion_sound():
+	var explosion_sound = AudioStreamPlayer2D.new()
+	explosion_sound.stream = load("res://assets/audio/sfx/AsteroidExplosion.wav")
+	explosion_sound.pitch_scale = 1
+	explosion_sound.position = self.position
+	get_parent().add_child(explosion_sound)
+	explosion_sound.play(0)
